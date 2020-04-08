bce1dd7b2b8c11901a3d0c1957d68c93809b623b
Mariam Haider <mfh.haider@gmail.com>
2020-04-08 01:52:36 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,17 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+require_relative '../lib/dancer'
+require_relative '../lib/class_methods_module'
+require_relative '../lib/dance_module'
+require_relative '../lib/kid'
+require_relative '../lib/fancy_dance.rb'
+
+
+class Dancer
+  include Dance      
+  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
