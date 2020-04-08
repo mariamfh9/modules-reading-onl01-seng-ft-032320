e208b91bb213cf41688a6b253e1af75de22bb387
Mariam Haider <mfh.haider@gmail.com>
2020-04-08 01:51:00 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,3 +1,8 @@
+require_relative '../lib/dancer'
+require_relative '../lib/class_methods_module'
+require_relative '../lib/dance_module'
+require_relative '../lib/kid'
+require_relative '../lib/fancy_dance.rb'
 
 
 class Dancer
